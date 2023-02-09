c3e05143164f3a900b08d03dcc7cec8e235046d8
Your Name <vesn1ana3@gmail.com>
2023-02-09 18:23:40 +0300

    del

--- 17.7.3.py
+++
@@ -1,7 +0,0 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money=int(input('Введите сумму, которую планируете положить на вклад '))
-deposit=[]
-for value in list(per_cent.values()):
-    deposit.append(int(value*money/100))
-print('deposit = ',deposit)
-print('Максимальная сумма, которую вы можете заработать — ',max(deposit))
